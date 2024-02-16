087fb5c84b892e376eb167077f35a638b95a56a4
Daniel Xavier <daniel.xavier@olxbr.com>
2024-02-16 10:11:54 -0300

    Update Manifest

--- workflow-reactions/manifest.ts
+++ workflow-reactions/manifest.ts
@@ -1,17 +1,10 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
-import GreetingWorkflow from "./workflows/greeting_workflow.ts";
+import AddReactionWorkflow from "./workflows/add_reaction_workflow.ts";
 
-/**
- * The app manifest contains the app's configuration. This
- * file defines attributes like app name and description.
- * https://api.slack.com/automation/manifest
- */
 export default Manifest({
   name: "workflow-reactions",
-  description:
-    "A sample that demonstrates using a function, workflow and trigger to send a greeting",
   icon: "assets/default_new_app_icon.png",
-  workflows: [GreetingWorkflow],
+  workflows: [AddReactionWorkflow],
   outgoingDomains: [],
-  botScopes: ["commands", "chat:write", "chat:write.public"],
+  botScopes: ["commands", "reactions:write"],
 });
