e80f435294eccaa3cb99c3ab8d7ebf22cb044c3a
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-28 16:01:20 -0300

    refactor: some code improvements by Prettier

--- workflow-reactions/functions/add_reaction_function.ts
+++ workflow-reactions/functions/add_reaction_function.ts
@@ -2,38 +2,38 @@ import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 
 export const AddReactionFunctionDefinition = DefineFunction({
   callback_id: "add_reaction_function",
-  title: "Add a reaction", // Exibido na sidebar dos steps
+  title: "Add a reaction",
   source_file: "functions/add_reaction_function.ts",
-  input_parameters: { // Exibidos dentro do input na configuração do step
+  input_parameters: {
     properties: {
       message_context: {
         type: Schema.slack.types.message_context,
-        description: "Message to react", 
+        description: "Message to react",
       },
       emoji_name: {
         type: Schema.types.string,
         description: "Emoji used in reaction",
       },
     },
-    required: ["message_context", "emoji_name"]
-  }
+    required: ["message_context", "emoji_name"],
+  },
 });
 
 export default SlackFunction(
   AddReactionFunctionDefinition,
-  
+
   ({ inputs, client }) => {
     const { message_context, emoji_name } = inputs;
     const { channel_id, message_ts } = message_context;
 
-    const parsed_emoji_name = emoji_name.replaceAll(':', '');
+    const parsed_emoji_name = emoji_name.replaceAll(":", "");
 
     client.reactions.add({
       channel: channel_id,
       name: parsed_emoji_name,
-      timestamp: message_ts
-    })
+      timestamp: message_ts,
+    });
 
     return { outputs: {} };
-  },
+  }
 );
--- workflow-reactions/workflows/add_reaction_workflow.ts
+++ workflow-reactions/workflows/add_reaction_workflow.ts
@@ -8,39 +8,33 @@ const AddReactionWorkflow = DefineWorkflow({
     properties: {
       interactivity: {
         type: Schema.slack.types.interactivity,
-      }
+      },
     },
     required: ["interactivity"],
   },
 });
 
-const inputForm = AddReactionWorkflow.addStep(
-  Schema.slack.functions.OpenForm,
-  {
-    title: "Add a reaction - Params",
-    interactivity: AddReactionWorkflow.inputs.interactivity,
-    fields: {
-      elements: [
-        {
-          name: "message_context",
-          type: Schema.types.object
-        },
-        {
-          name: "emoji_name",
-          type: Schema.types.string
-        }
-      ],
-      required: ["message_context", "emoji_name"],
-    },
+const inputForm = AddReactionWorkflow.addStep(Schema.slack.functions.OpenForm, {
+  title: "Add a reaction - Params",
+  interactivity: AddReactionWorkflow.inputs.interactivity,
+  fields: {
+    elements: [
+      {
+        name: "message_context",
+        type: Schema.types.object,
+      },
+      {
+        name: "emoji_name",
+        type: Schema.types.string,
+      },
+    ],
+    required: ["message_context", "emoji_name"],
   },
-);
+});
 
-AddReactionWorkflow.addStep(
-  AddReactionFunctionDefinition,
-  {
-    message_context: inputForm.outputs.fields.message_context,
-    emoji_name: inputForm.outputs.fields.emoji_name,
-  },
-);
+AddReactionWorkflow.addStep(AddReactionFunctionDefinition, {
+  message_context: inputForm.outputs.fields.message_context,
+  emoji_name: inputForm.outputs.fields.emoji_name,
+});
 
 export default AddReactionWorkflow;
